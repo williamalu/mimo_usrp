fa4d4f1061330aaa76b8ce10d78988b47fa0e093
Byron Wasti <byron.wasti@gmail.com>
2017-04-14 12:12:34 -0400

    Uses Numpy.complex64 as the data type

--- dataGeneration.py
+++ dataGeneration.py
@@ -2,7 +2,7 @@ import numpy as np
 
 V = 100
 
-x = np.array([V, -V, V, V, V, -V, -V, V, V, -V, -V, -V, -V], dtype=complex)
+x = np.array([V, -V, V, V, V, -V, -V, V, V, -V, -V, -V, -V], dtype=np.complex64)
 
 with open("data.bin", "wb") as f:
     f.write(x.tobytes())
