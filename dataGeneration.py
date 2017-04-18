74be0a9c50a41e31c675ddae90727f65917bcf66
William Lu <william.a.lu@gmail.com>
2017-04-18 11:44:43 -0400

    made dataGeneration a function and added comments

--- dataGeneration.py
+++ dataGeneration.py
@@ -1,13 +1,20 @@
 import numpy as np
 
-data_length = 1000
+def dataGeneration(data_length):
+    
+    # generate array of random binary
+    data = np.random.randint(2, size=data_length)
 
-data = np.random.randint(2, size=data_length)
-data = np.array_str(data, max_line_width=2*data_length)
+    # turn array into string
+    data = np.array_str(data, max_line_width=2*data_length)
 
-data = data.replace("[", "")
-data = data.replace("]", "")
-data = data.replace(" ", "")
+    # replace non-binary characters
+    data = data.replace("[", "")
+    data = data.replace("]", "")
+    data = data.replace(" ", "")
 
-with open("data.txt", "w") as f:
-    f.write(data)
+    with open("data.txt", "w") as f:
+         f.write(data)
+
+if __name__ == "__main__":
+    dataGeneration(1000)
