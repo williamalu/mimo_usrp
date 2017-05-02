0a3192cd5e9b42cb90b3561aa6fcf35ff8961aff
Byron Wasti <byron.wasti@gmail.com>
2017-05-01 23:21:48 -0400

    Fixes gen_data and gen_noise

--- scripts/gen_noise.py
+++ scripts/gen_noise.py
@@ -5,15 +5,15 @@ if __name__ == "__main__":
     data_path = '../data/'
 
     np.random.seed(45)
-    noise1 = np.random.normal(scale=0.2, size=100000);
-    noise2 = np.random.normal(scale=0.2, size=100000);
+    noise1 = np.random.normal(scale=0.2, size=100000)
+    noise2 = np.random.normal(scale=0.2, size=100000)
 
     noise1.tofile(data_path + "noise_1.bin")
     noise2.tofile(data_path + "noise_2.bin")
 
-    zeros_gap = np.zeros(10000);
+    zeros_gap = np.zeros(10000)
 
-    zeros = np.zeros(len(noise1));
+    zeros = np.zeros(len(noise1))
 
     channel1 = np.concatenate( [noise1, zeros_gap, zeros] )
     channel2 = np.concatenate( [zeros, zeros_gap, noise2] )
