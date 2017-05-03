459e73b4659d9cb3978525c69c0414ae862ec3fa
Shane Kelly <skelly1@lappy-14.04>
2017-05-02 21:42:48 -0400

    ADD: U matrix multiplication into decoder

--- scripts/decoder.py
+++ scripts/decoder.py
@@ -14,22 +14,32 @@ j = (0 + 1j)
 
 class Decoder(object):
 
-    def __init__(self, data_path, filename):
+    def __init__(self, data_path, received_data_filename_1,
+            received_data_filename_2, U_matrix):
 
         self.data_path = data_path
-        self.filename = filename
-        self.data_raw = None
+        self.received_data_filename_1 = received_data_filename_1
+        self.received_data_filename_2 = received_data_filename_2
+        self.data_raw_1 = None
+        self.data_raw_2 = None
         self.frequency_offset = None
         self.phase_offset = None
-        self.data_fixed = None
+        self.data_fixed_1 = None
+        self.data_fixed_2 = None
 
         self.samples = None
+
+        self.U_matrix = U_matrix
 
 
     def read_file(self):
 
-        file_path = self.data_path + self.filename
-        self.data_raw = np.fromfile(file_path, dtype=np.complex64)
+        file_path_1 = self.data_path + self.received_data_filename_1
+        file_path_2 = self.data_path + self.received_data_filename_2
+        self.data_raw_1 = np.fromfile(file_path_1, dtype=np.complex64)
+        self.data_raw_2 = np.fromfile(file_path_2, dtype=np.complex64)
+        self.data_fixed_1 = self.data_raw_1
+        self.data_fixed_2 = self.data_raw_2
 
 
     def find_offsets_bpsk(self):
@@ -65,12 +75,33 @@ class Decoder(object):
     def plot_data(self):
 
         plt.figure()
-        plt.plot(self.data_fixed.real, '.', label='real')
-        plt.plot(self.data_fixed.imag, '.', label='imag')
+        plt.subplot(2, 1, 1)
+        plt.plot(self.data_raw_1.real, label='real')
+        plt.plot(self.data_raw_1.imag, label='imag')
+        plt.title('Received Raw 1')
+        plt.legend()
+        plt.subplot(2, 1, 2)
+        plt.plot(self.data_fixed_1.real, label='real')
+        plt.plot(self.data_fixed_1.imag, label='imag')
+        plt.title('Received Fixed 1')
+        plt.legend()
+
+        plt.figure()
+        plt.subplot(2, 1, 1)
+        plt.plot(self.data_raw_2.real, label='real')
+        plt.plot(self.data_raw_2.imag, label='imag')
+        plt.title('Received Raw 2')
+        plt.legend()
+        plt.subplot(2, 1, 2)
+        plt.plot(self.data_fixed_2.real, label='real')
+        plt.plot(self.data_fixed_2.imag, label='imag')
+        plt.title('Received Fixed 2')
+        plt.legend()
         plt.show()
 
     
     def sample_data(self, T):
+
         oldval = None
         start_index = None
         for i, val in enumerate(self.data_fixed):
@@ -95,22 +126,39 @@ class Decoder(object):
 
     
     def is_data_correct(self):
+
         true_data = np.loadtxt('data_in_binary.txt')
 
         errors = true_data - self.samples
         print( np.count_nonzero(errors) )
 
 
+    def apply_U(self):
+        
+        # Multiply by respective V-vector value for MIMO
+        for i in range(len(self.data_fixed_1)):
+            data_vector = np.matrix([[self.data_fixed_1[i]],
+                    [self.data_fixed_2[i]]])
+            result = self.U_matrix * data_vector
+
+            self.data_fixed_1[i] = result[0]
+            self.data_fixed_2[i] = result[1]
+
+
 if __name__ == "__main__":
 
     data_path = '../data/'
-    input_filename = 'send_1.bin'
+    received_data_filename_1 = 'received_1_trimmed.bin'
+    received_data_filename_2 = 'received_2_trimmed.bin'
+    U_matrix = np.matrix.getH(np.matrix(np.load(data_path + 'U_approx.npy')))
 
-    decoder = Decoder(data_path, input_filename)
+    decoder = Decoder(data_path, received_data_filename_1,
+            received_data_filename_2, U_matrix)
     decoder.read_file()
-    decoder.find_offsets_bpsk()
-    decoder.fix_offsets()
-    #decoder.plot_data()
+    # decoder.find_offsets_bpsk()
+    # decoder.fix_offsets()
+    decoder.apply_U()
+    decoder.plot_data()
 
-    samples = decoder.sample_data(200)
-    decoder.is_data_correct()
+    # samples = decoder.sample_data(200)
+    # decoder.is_data_correct()
